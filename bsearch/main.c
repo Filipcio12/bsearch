50d7b0d8e36eacda4e62d76258704f4af9ac4791
Filipcio12 <121309536+Filipcio12@users.noreply.github.com>
2023-05-02 13:37:00 +0200

    Add files via upload

--- bsearch/main.c
+++ bsearch/main.c
@@ -3,27 +3,41 @@
 void* bsearch( const void *key, const void *ptr, size_t count, size_t size,
                int (*comp)(const void*, const void*) );
 
-int int_cmp(const void* a, const void* b);
+int intcmp(const void* a, const void* b);
 
 int main()
 {
     int arr[] = {1, 4, 11, 19, 33, 42, 48, 55};
+    int (*cmpPtr)(const void* a, const void* b) = &intcmp;
+
+    int* item;
+    int num = 11;
+    
+    item = (int*) bsearch(&num, arr, sizeof(arr) / sizeof(arr[0]), sizeof(int), cmpPtr);
+
+    if (item == NULL) {
+        printf("Item not found.");
+    }
+    else {
+        printf("Index of the found item = %ld.", item - arr);
+    }
     return 0;
 }
 
 void* bsearch( const void *key, const void *ptr, size_t count, size_t size,
                int (*comp)(const void*, const void*) )
 {
+    char* p = (char*) ptr; // char has a size of 1 byte
     int left = 0;
     int right = count - 1;
 
     while (left <= right) {
         int mid = (left + right) / 2;
 
-        if (comp(key, ptr + mid) == 0) {
-            return ptr + mid;
+        if (comp(key, (void*)(p + mid * size)) == 0) {
+            return (void*)(p + mid * size);
         }
-        else if (comp(key, ptr + mid) > 0) {
+        else if (comp(key, (void*)(p + mid * size)) > 0) {
             left = mid + 1;
         }
         else {
@@ -34,7 +48,7 @@ void* bsearch( const void *key, const void *ptr, size_t count, size_t size,
     return NULL;
 }
 
-int int_cmp(const void* a, const void* b)
+int intcmp(const void* a, const void* b)
 {
-    
+    return (*(int*)a - *(int*)b);
 }
